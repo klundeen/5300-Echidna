8a6be68fe30f75c01dbeea49d52fc62408bfe613
Bruno Ruiz de Somocurcio <44142527+BrunoRDS@users.noreply.github.com>
2021-05-27 18:20:28 -0700

    Cleaning Up code before submitting milestone 5

--- SQLExec.cpp
+++ SQLExec.cpp
@@ -257,7 +257,7 @@ QueryResult *SQLExec::select(const SelectStatement *statement) {
             projected_columns_names->push_back(Identifier(statement->selectList->at(i)->name));
         }
         *projected_column_attributes = *table.get_column_attributes(*projected_columns_names);
-        ColumnNames *projected_columns_names_copy = ColumnNames(*projected_columns_names);
+        ColumnNames projected_columns_names_copy = ColumnNames(*projected_columns_names);
         plan = new EvalPlan(projected_columns_names_copy, plan);
     }
 

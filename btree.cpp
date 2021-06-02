3dbc55439742a529f46c398bca135934b1248dfe
Bruno Ruiz de Somocurcio <44142527+BrunoRDS@users.noreply.github.com>
2021-06-01 18:20:20 -0700

    Memory Leaks fixed, Test size made smaller in btree.cpp

--- btree.cpp
+++ btree.cpp
@@ -108,7 +108,9 @@ Insertion BTreeIndex::_insert(BTreeNode *node, uint height, const KeyValue *key,
         return leaf->insert(key, handle);
     } else {
         auto *interior = dynamic_cast<BTreeInterior *>(node);
+        auto *found = interior->find(key, height);
         Insertion insertion = _insert(interior->find(key, height), height - 1, key, handle);
+        delete found;
         if (!BTreeNode::insertion_is_none(insertion))
             insertion = interior->insert(&insertion.second, insertion.first);
         return insertion;
@@ -156,7 +158,7 @@ bool test_btree() {
     row2["b"] = Value(101);
     table.insert(&row1);
     table.insert(&row2);
-    for (int i = 0; i < 100 * 1000; i++) {
+    for (int i = 0; i < 1000; i++) {
         ValueDict row;
         row["a"] = Value(i + 100);
         row["b"] = Value(-i);
